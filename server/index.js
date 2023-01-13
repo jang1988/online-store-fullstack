4ef606ab6f6e35e256f26cdfdf267b57f491b063
Baklay Petro <pustota1988@gmail.com>
2023-01-13 20:52:29 +0200

    createModels

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
-import * as dotenv from 'dotenv';
-import express from 'express';
-
+import dotenv from 'dotenv';
 dotenv.config();
+import express from 'express';
+import sequelize from './db.js';
+import models from './models/models.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log('error: ', error);
+  }
+};
+
+start();
