56195b9f14d08572133ff05e1a6c0bab99ff55e4
Baklay Petro <pustota1988@gmail.com>
2023-01-16 16:48:55 +0200

    createNavBar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,38 @@
 import React, { useContext } from 'react';
-import { Context } from '..';
+import { Context } from '../index';
+import Navbar from 'react-bootstrap/navbar';
+import Nav from 'react-bootstrap/nav';
+import Container from 'react-bootstrap/container';
+import { Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { SHOP_ROUTE } from '../utils/consts';
+import { observer } from 'mobx-react-lite';
 
-const Navbar = () => {
-  const {user} = useContext(Context)
+const NavBar = observer(() => {
+  const { user } = useContext(Context);
+  console.log('user: ', user);
+
   return (
-    <></>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <NavLink style={{ color: 'white', fontSize: 30, textDecoration: 'none' }} to={SHOP_ROUTE}>
+          MY-SHOP
+        </NavLink>
+        {user.isAuth ? (
+          <Nav className="ml-auto" style={{ color: 'white' }}>
+            <Button variant={'outline-light'}>Админ панель</Button>
+            <Button variant={'outline-light'} className="ms-2">Выйти</Button>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto" style={{ color: 'white' }}>
+            <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>
+              Авторизация
+            </Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
   );
-}
+});
 
-export default Navbar;
+export default NavBar;
